44ef22341299d61b06c1d4974d7fe97457e870e6
Mark Richards <mrich@centos70.localdomain>
2017-04-09 19:19:42 -0400

    Add first cut of mctrainer

---
+++ speedym/flash/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	url(r'^listing', views.listing, name='listing'),
+	url(r'^jsonlisting', views.jsonlisting, name='jsonlisting'),
+	url(r'^iterlist', views.iterlist, name='iterlist'),
+	url(r'^subsetq', views.subsetq, name='subsetq'),
+	url(r'^postarg', views.postarg, name='postarg'),
+	url(r'^postbout', views.postBout, name='postBout'),
+]
